f87b9595aec5b955f5da6dd176ce7865d17b81bb
arianpollozhani <ap28647@seeu.edu.mk>
2023-06-15 14:55:10 +0200

    Update app.py

--- Moisture project/app.py
+++ Moisture project/app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
-import sqlite3
+import pymysql
+
 app = Flask(__name__)
+
+# Database configuration
+host = 'localhost'
+user = 'your_username'
+password = 'your_password'
+database = 'your_database_name'
+
 @app.route('/')
 def index():
-    # Connect to the SQLite database
-    conn = sqlite3.connect('plants.db')
+    # Connect to the MariaDB database
+    conn = pymysql.connect(host=host, user=user, password=password, database=database)
     cursor = conn.cursor()
 
     # Retrieve data from the table
@@ -13,5 +21,6 @@ def index():
 
     # Render the template with the retrieved data
     return render_template('index.html', rows=rows)
+
 if __name__ == '__main__':
     app.run(debug=True)
